e771da7ed318bc130edc58acf85ec4e6ad7b0994
leftthomas <leftthomas@qq.com>
2017-12-27 16:37:18 +0800

    change parameter col

--- capsule.py
+++ capsule.py
@@ -17,7 +17,7 @@ class CapsuleLayer(nn.Module):
         self.num_capsules = num_capsules
 
         if num_route_nodes != -1:
-            self.route_weights = nn.Parameter(torch.randn(num_capsules, num_route_nodes, in_channels, out_channels))
+            self.route_weights = nn.Parameter(torch.randn(num_capsules, num_route_nodes, out_channels, in_channels))
         else:
             self.capsules = nn.ModuleList(
                 [nn.Conv2d(in_channels, out_channels, kernel_size=kernel_size, stride=stride, padding=0) for _ in
@@ -31,12 +31,13 @@ class CapsuleLayer(nn.Module):
 
     def forward(self, x):
         if self.num_route_nodes != -1:
-            priors = x.unsqueeze(dim=-2).unsqueeze(dim=0).matmul(self.route_weights.unsqueeze(dim=1))
+            x = x.unsqueeze(dim=-1).unsqueeze(dim=1)
+            priors = self.route_weights.unsqueeze(dim=0).matmul(x).squeeze(dim=-1)
             logits = Variable(torch.zeros(*priors.size()))
             if torch.cuda.is_available():
                 logits = logits.cuda()
             for i in range(self.num_iterations):
-                probs = F.softmax(logits, dim=2)
+                probs = F.softmax(logits, dim=1)
                 outputs = self.squash((probs * priors).sum(dim=2, keepdim=True))
 
                 if i != self.num_iterations - 1:
